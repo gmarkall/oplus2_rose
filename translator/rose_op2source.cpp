141472f3dafbd5780ff4430d4b087de6b0bc61cd
gmarkall <grm08@doc.ic.ac.uk>
2011-01-20 11:07:15 +0000

    Tidy up rose_op2source, in the same way as other commits.

--- translator/rose_op2source.cpp
+++ translator/rose_op2source.cpp
@@ -1,17 +1,32 @@
 /*
-
-Copyright (c) 2010, Graham Markall
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of Imperial College London nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+ * 
+ * Copyright (c) 2010, Graham Markall
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of Imperial College London nor the names of its
+ *       contributors may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 
 #include <iostream>
 #include <sstream>
@@ -20,14 +35,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include "rose_op2parloop.h"
 #include "rose_op2argument.h"
 
-///////////////////////////// OPSource : Implementation /////////////////////////////////////////////////////////////////////
-
-/*
- * Empty default constructor
- */
-OPSource::OPSource() 
-{  
-}
+////////////// OPSource : Implementation ///////////////////////////////////////
 
 /* 
  * Source needs to know which ROSE project it is working on
@@ -48,49 +56,49 @@ void OPSource::fixParLoops(SgNode *n)
   {
     string fn_name = fn->getAssociatedFunctionDeclaration()->get_name().getString();
     if(fn_name.compare("op_par_loop_2")==0 
-		|| fn_name.compare("op_par_loop_3")==0 
-		|| fn_name.compare("op_par_loop_4")==0
-		|| fn_name.compare("op_par_loop_5")==0
-  	|| fn_name.compare("op_par_loop_6")==0
-		|| fn_name.compare("op_par_loop_7")==0
-		|| fn_name.compare("op_par_loop_8")==0
-		|| fn_name.compare("op_par_loop_9")==0) 
+    || fn_name.compare("op_par_loop_3")==0 
+    || fn_name.compare("op_par_loop_4")==0
+    || fn_name.compare("op_par_loop_5")==0
+    || fn_name.compare("op_par_loop_6")==0
+    || fn_name.compare("op_par_loop_7")==0
+    || fn_name.compare("op_par_loop_8")==0
+    || fn_name.compare("op_par_loop_9")==0) 
     {
       SgExprListExp* exprList = fn->get_args();
       SgExpressionPtrList &exprs = exprList->get_expressions();
       SgFunctionRefExp* varExp =  isSgFunctionRefExp(exprs[0]);
       if(varExp != NULL)
       {
-      	kernel_name = varExp->get_symbol()->get_name();
+        kernel_name = varExp->get_symbol()->get_name();
       }
       exprs.erase(exprs.begin());
 
-			SgExpressionPtrList::iterator it = exprs.begin() + op_par_loop::num_params - 1;
-			for(; it != exprs.end(); it += op_argument::num_params)
-			{
-				*it = buildCastExp( *it, buildPointerType(SgClassType::createType( buildStructDeclaration("op_dat<void>"))) );
-			}
+      SgExpressionPtrList::iterator it = exprs.begin() + op_par_loop::num_params - 1;
+      for(; it != exprs.end(); it += op_argument::num_params)
+      {
+        *it = buildCastExp( *it, buildPointerType(SgClassType::createType( buildStructDeclaration("op_dat<void>"))) );
+      }
 
-			// Inject Name
-			exprs.insert(exprs.begin(), buildStringVal(kernel_name));
-			
-			// Fetch the declaration
+      // Inject Name
+      exprs.insert(exprs.begin(), buildStringVal(kernel_name));
+      
+      // Fetch the declaration
       SgName name = SgName("op_par_loop_") + kernel_name;
       SgFunctionDeclaration *funcDecl = cudaFunctionDeclarations[kernel_name];
       if(funcDecl)
       {
-		  	SgFunctionRefExp* ref = isSgFunctionRefExp(fn->get_function());
-		  	SgFunctionSymbol *symbol = ref->get_symbol();
-		  	symbol->set_declaration(funcDecl);
-		  	ref->set_symbol(symbol);
-		  	fn->set_function(ref);
+        SgFunctionRefExp* ref = isSgFunctionRefExp(fn->get_function());
+        SgFunctionSymbol *symbol = ref->get_symbol();
+        symbol->set_declaration(funcDecl);
+        ref->set_symbol(symbol);
+        fn->set_function(ref);
       }
     }
   }
 }
 
 /*
- *	Fix op structure calls and inject debug names
+ *  Fix op structure calls and inject debug names
  */
 void OPSource::fixOpFunctions(SgNode *n)
 {
@@ -102,28 +110,28 @@ void OPSource::fixOpFunctions(SgNode *n)
     if(fn_name.compare("op_decl_const")==0) 
     {
       SgExprListExp* exprList = fn->get_args();
-			SgExpressionPtrList &exprs = exprList->get_expressions();
-			if( isSgStringVal(exprs.back()) == NULL )
-			{
-		    SgVarRefExp* varExp = isSgVarRefExp(exprs[1]);
-				if(!varExp)
-				{
-					varExp = isSgVarRefExp(isSgAddressOfOp(exprs[1])->get_operand_i());
-				}
+      SgExpressionPtrList &exprs = exprList->get_expressions();
+      if( isSgStringVal(exprs.back()) == NULL )
+      {
+        SgVarRefExp* varExp = isSgVarRefExp(exprs[1]);
+        if(!varExp)
+        {
+          varExp = isSgVarRefExp(isSgAddressOfOp(exprs[1])->get_operand_i());
+        }
 
-				if(varExp)
-		    {
-		    	var_name = varExp->get_symbol()->get_name();
-		    }
-				cout << "---Injecting Debug Name for const: " << var_name.getString() << "---" << endl;
-		    exprList->append_expression(buildStringVal(var_name));
-			}
+        if(varExp)
+        {
+          var_name = varExp->get_symbol()->get_name();
+        }
+        cout << "---Injecting Debug Name for const: " << var_name.getString() << "---" << endl;
+        exprList->append_expression(buildStringVal(var_name));
+      }
     }
   }
 }
 
 /*
- *	Fix OP function calls and inject debug names
+ *  Fix OP function calls and inject debug names
  */
 void OPSource::fixOpStructs(SgNode *n)
 {
@@ -135,20 +143,19 @@ void OPSource::fixOpStructs(SgNode *n)
     if(initer)
     {
       string class_name = initer->get_class_decl()->get_name().getString();
-			//cout << "------------Constructors---------------------" << class_name << "---" << endl;
-			if(class_name.find("op_dat") != string::npos
-					|| class_name.find("op_dat_gbl") != string::npos
-					|| class_name.compare("_op_ptr") == 0 
-				  || class_name.compare("_op_set") == 0 
-				  || class_name.compare("_op_dat_const") == 0)
-			{
+      if(class_name.find("op_dat") != string::npos
+          || class_name.find("op_dat_gbl") != string::npos
+          || class_name.compare("_op_ptr") == 0 
+          || class_name.compare("_op_set") == 0 
+          || class_name.compare("_op_dat_const") == 0)
+      {
         cout << "---Injecting Debug Name: " << var_name << "---" << endl;
         SgExprListExp* list = initer->get_args();
-				SgExpressionPtrList &exprs = list->get_expressions();
-				if( isSgStringVal(exprs.back()) == NULL )
-				{
-        	list->append_expression(buildStringVal(var_name));
-				}
+        SgExpressionPtrList &exprs = list->get_expressions();
+        if( isSgStringVal(exprs.back()) == NULL )
+        {
+          list->append_expression(buildStringVal(var_name));
+        }
       }
     }
   }
@@ -163,7 +170,7 @@ void OPSource::visit(SgNode *n)
   SgFile *file = isSgFile(n);
   if(file!=NULL)
   {
-	file->set_unparse_includes(false);
+  file->set_unparse_includes(false);
   }
 
   // We need to put the global scope on the scope stack so that we can look
@@ -179,7 +186,7 @@ void OPSource::visit(SgNode *n)
 
   fixParLoops(n);
   fixOpStructs(n);
-	fixOpFunctions(n);
+  fixOpFunctions(n);
 }
 
 /* 

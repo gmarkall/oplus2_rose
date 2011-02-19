ffce0fc43e7bd3177e0273e552a2f67d872b3365
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-02-19 19:50:55 +0000

    use operator() instead of operator[] to access mapped data in kernels

--- tests/laplace1d/laplace.h
+++ tests/laplace1d/laplace.h
@@ -6,8 +6,8 @@
 ///             x coordinates of element vertices
 template < typename T >
 inline void laplace(Data1D<T>& v, const Data1D<T>& w, const Data1D<T>& x) {
-  const T hinv = 1./(x[1] - x[0]);
-  v[0] = hinv*(w[0]-w[1]);
-  v[1] = hinv*(w[1]-w[0]);
+  const T hinv = 1./(x(1) - x(0));
+  v(0) = hinv*(w(0)-w(1));
+  v(1) = hinv*(w(1)-w(0));
 }
 
--- tests/laplace1d/laplace_mat.h
+++ tests/laplace1d/laplace_mat.h
@@ -5,10 +5,10 @@
 ///             x coordinates of element vertices
 template < typename T >
 inline void laplace(Data1D<T>& A, const Data1D<T>& x) {
-  const T hinv = 1./(x[1] - x[0]);
-  A[0] = hinv;
-  A[1] = -hinv;
-  A[2] = -hinv;
-  A[3] = hinv;
+  const T hinv = 1./(x(1) - x(0));
+  A(0) = hinv;
+  A(1) = -hinv;
+  A(2) = -hinv;
+  A(3) = hinv;
 }
 
